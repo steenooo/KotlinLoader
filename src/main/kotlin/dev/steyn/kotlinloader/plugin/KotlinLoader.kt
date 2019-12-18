8b6264f50e56f423407683edece278276255f91d
Frederick <steenooo5@gmail.com>
2019-12-18 22:22:13 +0100

    Removed unnecessary check for pluginmanager type.

--- src/main/kotlin/dev/steyn/kotlinloader/plugin/KotlinLoader.kt
+++ src/main/kotlin/dev/steyn/kotlinloader/plugin/KotlinLoader.kt
@@ -3,7 +3,6 @@ package dev.steyn.kotlinloader.plugin
 import dev.steyn.kotlinloader.bootstrap.KotlinLoaderPlugin
 import dev.steyn.kotlinloader.loader.KotlinPluginLoader
 import org.bukkit.Bukkit
-import org.bukkit.plugin.SimplePluginManager
 import java.io.File
 
 class KotlinLoader(val plugin: KotlinLoaderPlugin) {
@@ -12,9 +11,6 @@ class KotlinLoader(val plugin: KotlinLoaderPlugin) {
     fun load() {
 
         val manager = Bukkit.getPluginManager()
-        if (manager !is SimplePluginManager) {
-            throw Exception("Invalid PluginManager type")
-        }
         manager.registerInterface(KotlinPluginLoader::class.java)
         val pluginsFolder = File(plugin.dataFolder.parent, "kotlin")
         if (!pluginsFolder.exists()) {
